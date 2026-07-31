b454289b5d5bdb4dd83912596afba0cb56a0a2e1
Dharshini2803 <155939364+Dharshini2803@users.noreply.github.com>
2026-07-31 21:36:05 +0530

    Time: 9 ms (94.22%), Space: 48.3 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumPushes(String word) {
+        
+        int[] frequency = new int[26];
+        for (char c : word.toCharArray()) {
+            frequency[c - 'a']++;
+        }
+        
+        
+        Arrays.sort(frequency);
+        
+        int totalPushes = 0;
+        int distinctLettersProcessed = 0;
+        
+        for (int i = 25; i >= 0; i--) {
+            if (frequency[i] == 0) {
+                break; // No more letters left in the word
+            }
+            
+    
+            int pushesPerPress = (distinctLettersProcessed / 8) + 1;
+            totalPushes += frequency[i] * pushesPerPress;
+            
+            distinctLettersProcessed++;
+        }
+        
+        return totalPushes;
+    }
+}
